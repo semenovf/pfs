f959647d272739ec767dbf4f71933923509c2241
Semenov Fedor <fedor.v.semenov@gmail.com>
2017-03-16 21:53:24 +0500

    Implementing str_to_uintmax().

---
+++ include/pfs/compiler/gnuc/ctype.hpp
@@ -0,0 +1,196 @@
+/* 
+ * File:   ctype.hpp
+ * Author: wladt
+ *
+ * Created on March 16, 2017, 7:34 PM
+ */
+
+#ifndef __PFS_COMPILER_GNUC_CTYPE_HPP__
+#define __PFS_COMPILER_GNUC_CTYPE_HPP__
+
+#include <cctype>
+#include <cwctype>
+#include <locale>
+
+namespace pfs {
+
+//
+// ISALPHA(3) man page
+// ...
+// POSIX.1-2008 marks isascii() as obsolete, noting that it cannot 
+// be used portably in a localized application.
+// ...
+//
+
+template <typename CharT>
+bool is_alnum (CharT c);
+
+template <typename CharT>
+bool is_alpha (CharT c);
+
+template <typename CharT>
+bool is_cntrl (CharT c);
+
+template <typename CharT>
+bool is_digit (int c);
+
+template <typename CharT>
+bool is_graph (CharT c);
+
+template <typename CharT>
+bool islower (CharT c);
+
+template <typename CharT>
+bool is_print (CharT c);
+
+template <typename CharT>
+bool is_punct (CharT c);
+
+template <typename CharT>
+bool is_space (CharT c);
+
+template <typename CharT>
+bool is_upper (CharT c);
+
+template <typename CharT>
+bool is_xdigit (CharT c);
+
+
+template <>
+inline bool is_alnum<char> (char c)
+{
+    return std::isalnum(c);
+}
+
+template <>
+inline bool is_alpha<char> (char c)
+{
+    return std::isalpha(c);
+}
+
+template <>
+inline bool is_cntrl<char> (char c)
+{
+    return std::iscntrl(c);
+}
+
+template <>
+inline bool is_digit<char>(char c)
+{
+    return std::isdigit(c);
+}
+
+template <>
+inline bool is_graph<char> (char c)
+{
+    return std::isgraph(c);
+}
+
+template <>
+inline bool is_lower<char> (char c)
+{
+    return std::islower(c);
+}
+
+template <>
+inline bool is_print<char> (char c)
+{
+    return std::isprint(c);
+}
+
+template <>
+inline bool is_punct<char> (char c)
+{
+    return std::ispunct(c);
+}
+
+template <>
+inline bool is_space<char> (char c)
+{
+    return std::isspace(c);
+}
+
+template <>
+inline bool is_upper<char> (char c)
+{
+    return std::isupper(c);
+}
+
+template <>
+inline bool is_xdigit<char> (char c)
+{
+    return std::isxdigit(c);
+}
+
+//---
+
+template <>
+inline bool is_alnum<wchar_t> (wchar_t c)
+{
+    return std::iswalnum(c);
+}
+
+template <>
+inline bool is_alpha<wchar_t> (wchar_t c)
+{
+    return std::iswalpha(c);
+}
+
+template <>
+inline bool is_cntrl<wchar_t> (wchar_t c)
+{
+    return std::iswcntrl(c);
+}
+
+template <>
+inline bool is_digit<wchar_t>(wchar_t c)
+{
+    return std::iswdigit(c);
+}
+
+template <>
+inline bool is_graph<wchar_t> (wchar_t c)
+{
+    return std::iswgraph(c);
+}
+
+template <>
+inline bool is_lower<wchar_t> (wchar_t c)
+{
+    return std::iswlower(c);
+}
+
+template <>
+inline bool is_print<wchar_t> (wchar_t c)
+{
+    return std::iswprint(c);
+}
+
+template <>
+inline bool is_punct<wchar_t> (wchar_t c)
+{
+    return std::iswpunct(c);
+}
+
+template <>
+inline bool is_space<wchar_t> (wchar_t c)
+{
+    return std::iswspace(c);
+}
+
+template <>
+inline bool is_upper<wchar_t> (wchar_t c)
+{
+    return std::iswupper(c);
+}
+
+template <>
+inline bool is_xdigit<wchar_t> (wchar_t c)
+{
+    return std::iswxdigit(c);
+}
+
+} // pfs
+
+#endif /* __PFS_COMPILER_GNUC_CTYPE_HPP__ */
+
--- include/pfs/lexical_cast.hpp
+++ include/pfs/lexical_cast.hpp
@@ -13,6 +13,9 @@
 #include <pfs/limits.hpp>
 #include <pfs/types.hpp>
 
+#include <pfs/compiler.hpp>
+#include PFS_CC_HEADER(ctype)
+
 namespace pfs {
 
 template <typename StringT>
@@ -30,68 +33,70 @@ public:
     {}
 };
 
-namespace lexical_cast_ns {
-        
-template <typename CharIteratorT>
-class checked_iterator
-{
-public:
-    typedef typename CharIteratorT::value_type value_type;
-    
-private:
-    CharIteratorT & _pos;
-    CharIteratorT & _endpos;
-    value_type      _badval;
-    
-public:
-    checked_iterator (CharIteratorT & beginpos
-            , CharIteratorT & endpos
-            , value_type badval = 0)
-        : _pos(beginpos)
-        , _endpos(endpos)
-        , _badval(badval)
-    {}
-    
-    checked_iterator & operator ++ () // prefix increment
-	{
-        if (_pos != _endpos)
-            ++_pos;
-        return this;
-	}
-
-    value_type operator * () const
-    {
-        return _pos == _endpos ? _badval : *_pos;
-    }
-
-    CharIteratorT & pos ()
-    {
-        return _pos;
-    }
-    
-    value_type value () const
-    {
-        return _pos == _endpos ? _badval : *_pos;
-    }
-
-    value_type next_value ()
-    {
-        if (_pos != _endpos) {
-            CharIteratorT p(_pos);
-            ++p;
-            return p == _endpos ? _badval : *p;
-        }
-        
-        return _badval;
-    }
-    
-    bool at_end () const
-    {
-        return _pos == _endpos;
-    }
-};
-
-} // lexical_cast_ns
+//namespace lexical_cast_ns {
+//        
+//template <typename CharIteratorT>
+//class checked_iterator
+//{
+//public:
+//    typedef typename CharIteratorT::value_type value_type;
+//    
+//private:
+//    CharIteratorT & _pos;
+//    CharIteratorT & _endpos;
+//    value_type      _badval;
+//    
+//public:
+//    checked_iterator (CharIteratorT & beginpos
+//            , CharIteratorT & endpos
+//            , value_type badval = 0)
+//        : _pos(beginpos)
+//        , _endpos(endpos)
+//        , _badval(badval)
+//    {}
+//    
+//    checked_iterator & operator ++ () // prefix increment
+//	{
+//        if (_pos != _endpos)
+//            ++_pos;
+//        return this;
+//	}
+//
+//    value_type operator * () const
+//    {
+//        return _pos == _endpos ? _badval : *_pos;
+//    }
+//
+//    bool operator == (CharIteratorT)
+//    
+//    CharIteratorT & pos ()
+//    {
+//        return _pos;
+//    }
+//    
+//    value_type value () const
+//    {
+//        return _pos == _endpos ? _badval : *_pos;
+//    }
+//
+//    value_type next_value ()
+//    {
+//        if (_pos != _endpos) {
+//            CharIteratorT p(_pos);
+//            ++p;
+//            return p == _endpos ? _badval : *p;
+//        }
+//        
+//        return _badval;
+//    }
+//    
+//    bool at_end () const
+//    {
+//        return _pos == _endpos;
+//    }
+//};
+//
+//} // lexical_cast_ns
 
 //
 // Grammar (case radix = 0)
@@ -128,102 +133,129 @@ template <typename CharIteratorT>
 uintmax_t string_to_uintmax (CharIteratorT beginpos
     , CharIteratorT endpos
     , CharIteratorT * badpos
-    , int radix
-    , bool * ok)
+    , int radix)
 {
     typedef typename CharIteratorT::value_type value_type;
     
-    lexical_cast_ns::checked_iterator<CharIteratorT> p(beginpos, endpos, 0);
+    //lexical_cast_ns::checked_iterator<CharIteratorT> pos(beginpos, endpos, 0);
+    CharIteratorT pos = beginpos;
     uintmax_t result = 0;
 
-    if (ok != 0)
-        *ok = false;
-    
-    if (radix != 0 && radix < 2) {
-        if (badpos != 0)
-            *badpos = p.pos();
+    do {
 
-        return 0;
-    }
+        // Error
+        if (radix != 0 && (radix < 2 || radix > 36))
+            break;
     
 //    unsigned char c;
-    uintmax_t cutoff;
-    int any, cutlim;
+//    uintmax_t cutoff;
+//    int any, cutlim;
 
-    // Skip white spaces
-    //
-    while (pfs::isspace(*p)) {
-        ++p;
-    }
-    
-    if (*p == '-') {
-        if (badpos != 0)
-            *badpos = p;
+        // Skip white spaces
+        //
+        while (pos != endpos && pfs::is_space(*pos))
+            ++pos;
         
-        return 0;
-    } 
-    
-    if (*p == '+')
-        ++p;
-    
-    // Determine radix
-    //
-    if (radix == 0 || radix == 16) {
-        value_type next_value = p.next_value();
-        
-        if (*p == '0' && (next_value == 'x' || next_value == 'X')) {
-            radix = 16;
-            ++p;
-            ++p;
-        }
-    }
-    
-    if (radix == 0) {
-        if (*p == '0') {
-            radix = 8;
-            ++p;
-        } else {
-            radix = 10;
-        }
-    }
-    
-    cutoff = numeric_limits<uintmax_t>.max() / static_cast<uintmax_t>(radix);
-    cutlim = numeric_limits<uintmax_t>.max() % static_cast<uintmax_t>(radix);
-    
-    for (result = 0, any = 0; pos != endpos; c = *s++) {
-        if (!pfs::isascii(c))
+        // Finish
+        if (pos == endpos)
             break;
-        
-        if (pfs::isdigit(c))
-            c -= '0';
-        else if (pfs::isalpha(c))
-            c -= pfs::isupper(c) ? 'A' - 10 : 'a' - 10;
-        else
+
+        // Finish
+        if (*pos == '-')
             break;
-        
-        if (c >= radix)
-            break;
-        
-        if (any < 0 || result > cutoff || (result == cutoff && c > cutlim))
-            any = -1;
-        else {
-            any = 1;
-            result *= static_cast<uintmax_t>(radix);
-            result += c;
+
+        if (*pos == '+') {
+            ++pos;
+            
+            // Finish
+            if (pos == endpos)
+                break;
         }
-    }
+
+
+        // Determine radix
+        //
+        if (radix == 0 || radix == 16) {
+            CharIteratorT nextpos = pos;
+            ++nextpos;
+
+            if (nextpos != endpos) {
+                if (*pos == '0' && (*nextpos == 'x' || *nextpos == 'X')) {
+                    radix = 16;
+                    ++pos;
+                    ++pos;
+
+                    // Finish
+                    if (pos == endpos)
+                        break;
+                }
+            }
+        }
+
+        if (radix == 0) {
+            if (*pos == '0') {
+                radix = 8;
+                ++pos;
+                
+                // Finish
+                if (pos == endpos)
+                    break;
+            } else {
+                radix = 10;
+            }
+        }
+
+        cutoff = numeric_limits<uintmax_t>.max() / static_cast<uintmax_t>(radix);
+        cutlim = numeric_limits<uintmax_t>.max() % static_cast<uintmax_t>(radix);
+
+        for (; pos != endpos; ++pos) {
+
+            value_type c = *pos;
+            int digit = 0;
+            
+            // Finish
+            if (!pfs::is_print(c))
+                break;
+
+            if (pfs::is_digit(c))
+                digit = c - '0';
+            else if (pfs::is_lower(c))
+                digit = c - 'a' - 10;
+            else if (pfs::is_upper(c))
+                digit = c - 'A' - 10;
+            else
+                break;
+
+            // Finish
+            if (digit >= radix)
+                break;
+
+            if (any < 0 || result > cutoff || (result == cutoff && c > cutlim))
+                any = -1;
+            else {
+                any = 1;
+                result *= static_cast<uintmax_t>(radix);
+                result += digit;
+            }
+        }
+
+        if (any == 0) {
+            if (ok != 0)
+                *ok = false;
+        } else if (any < 0) {
+            acc = ULLONG_MAX;
+            if (ok != 0)
+                *ok = false;
+        }
     
-    if (any == 0) {
-        if (ok != 0)
-            *ok = false;
-    } else if (any < 0) {
-        acc = ULLONG_MAX;
-        if (ok != 0)
-            *ok = false;
-    }
+        ok = true;
+    } while (false);
     
-    if (endptr != 0)
-        *endptr = (any ? s - 1 : nptr);
+    if (badpos != 0)
+        *badpos = pos;
+
+//    if (endptr != 0)
+//        *endptr = (any ? s - 1 : nptr);
     
     return result;
 }
@@ -232,8 +264,7 @@ template <typename CharIteratorT>
 intmax_t string_to_intmax (CharIteratorT beginpos
     , CharIteratorT endpos
     , CharIteratorT * badpos
-    , int radix
-    , bool * ok)
+    , int radix)
 {
     return 0;
 }
@@ -243,11 +274,18 @@ typename pfs::enable_if<pfs::is_integral<Integer>::value
         && pfs::is_unsigned<Integer>::value, Integer>::type
 lexical_cast (StringT const & s, int radix = 10)
 {
-    bool ok = true;
-    uintmax_t result = string_to_uintmax<StringT>(s, radix, & ok);
-    if (! ok)
+    typedef typename StringT::const_iterator const_iterator;
+    const_iterator badpos;
+    
+    uintmax_t result = string_to_uintmax<const_iterator>(s.cbegin()
+            , s.cend()
+            , & badpos
+            , radix);
+    
+    if (! badpos != s.cend())
         throw bad_lexical_cast<StringT>("lexical_cast()");
-    return 0;//result;
+    
+    return result;
 }
 
 template <typename StringT, typename Integer>
@@ -255,11 +293,18 @@ typename pfs::enable_if<pfs::is_integral<Integer>::value
         && pfs::is_signed<Integer>::value, Integer>::type
 lexical_cast (StringT const & s, int radix = 10)
 {
-    bool ok = true;
-    intmax_t result = lexical_cast_intmax<StringT>(s, radix, & ok);
-    if (! ok)
+    typedef typename StringT::const_iterator const_iterator;
+    const_iterator badpos;
+    
+    intmax_t result = string_to_intmax<const_iterator>(s.cbegin()
+            , s.cend()
+            , & badpos
+            , radix);
+    
+    if (! badpos != s.cend())
         throw bad_lexical_cast<StringT>("lexical_cast()");
-    return 0;//result;
+    
+    return result;
 }
 
 } // pfs
