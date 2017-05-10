21a394ce9beecd92837873d3082be585a04a19d7
Fedor Semenov <fedor.v.semenov@gmail.com>
2017-05-10 14:04:56 +0500

    Added static_assert for C++ prior to C++11

--- include/pfs/cxxlang.hpp
+++ include/pfs/cxxlang.hpp
@@ -84,5 +84,29 @@
 #   define pfs_maybe_unused
 #endif
 
+
+// static_assert (expression, message)
+//
+#if __cplusplus < 201103L
+
+// [STATIC_ASSERT](http://www.pixelbeat.org/programming/gcc/static_assert.html)
+
+#   define PFS_ASSERT_CONCAT_P(a, b) a##b
+#   define PFS_ASSERT_CONCAT(a, b) PFS_ASSERT_CONCAT_P(a, b)
+/* These can't be used after statements in c89. */
+#   ifdef __COUNTER__
+#       define static_assert(e,m) \
+            ;enum { ASSERT_CONCAT(static_assert_, __COUNTER__) = 1/(int)(!!(e)) }
+#   else
+  /* This can't be used twice on the same line so ensure if using in headers
+   * that the headers are not included twice (by wrapping in #ifndef...#endif)
+   * Note it doesn't cause an issue when used on same line of separate modules
+   * compiled with gcc -combine -fwhole-program.  */
+#       define static_assert(e,m) \
+            ;enum { PFS_ASSERT_CONCAT(assert_line_, __LINE__) = 1/(int)(!!(e)) }
+#   endif
+
+#endif
+
 #endif /* __PFS_CXXLANG_HPP__ */
 
