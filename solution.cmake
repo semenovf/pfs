dfb59186a0ee5aed0f272ceb7c4f41e83248a8a6
Semenov Fedor <fedor.v.semenov@gmail.com>
2017-11-27 18:14:48 +0500

    Minor changes.

---
+++ solution.cmake
@@ -0,0 +1,9 @@
+# Ubuntu 14.04 - VERSION 2.8.12
+cmake_minimum_required(VERSION 2.8.12)
+
+# [CMake with Qt4](https://wiki.qt.io/Using_CMake_build_system)
+find_package(Qt4 COMPONENTS QTCORE)
+
+# [CMake Manual](http://doc.qt.io/qt-5/cmake-manual.html)
+find_package(Qt5Core)
+find_package(Qt5Widgets)
