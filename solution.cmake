323f0e1ee453f9dc9add953a69048c0a7df777d4
Semenov Fedor <fedor.v.semenov@gmail.com>
2017-12-04 18:59:15 +0500

    Preparing cmake environment for solution (instead of pemake5).

--- solution.cmake
+++ solution.cmake
@@ -1,5 +1,25 @@
-# Ubuntu 14.04 - VERSION 2.8.12
-cmake_minimum_required(VERSION 2.8.12)
+include(CheckIncludeFileCXX) # For use of CHECK_INCLUDE_FILE_CXX()
+
+set(PFS_PREFERRED_CXX_STANDARD 98) # FIXME
+
+# pthread
+find_package(Threads)
+
+# [FindBoost](https://cmake.org/cmake/help/git-master/module/FindBoost.html)
+find_package(Boost COMPONENTS
+    system
+    filesystem
+    chrono
+    regex
+    thread)
+
+if(Boost_FOUND)
+#    include_directories(${Boost_INCLUDE_DIRS})
+    check_include_file_cxx("boost/ratio.hpp" HAVE_BOOST_RATIO)
+    check_include_file_cxx("boost/smart_ptr.hpp" HAVE_BOOST_SMART_PTR)
+    check_include_file_cxx("boost/filesystem.hpp" HAVE_BOOST_FILESYSTEM)
+endif(Boost_FOUND)
+
 
 # [CMake with Qt4](https://wiki.qt.io/Using_CMake_build_system)
 find_package(Qt4 COMPONENTS QTCORE)
@@ -7,3 +27,5 @@ find_package(Qt4 COMPONENTS QTCORE)
 # [CMake Manual](http://doc.qt.io/qt-5/cmake-manual.html)
 find_package(Qt5Core)
 find_package(Qt5Widgets)
+
+configure_file(${CMAKE_CURRENT_LIST_DIR}/config.h.in ${CMAKE_CURRENT_LIST_DIR}/config.h)
