cd2b02d2161f0e435d0b96a841f2bae5326901a8
Fedor Semenov <fedor.v.semenovf@gmail.com>
2018-04-18 18:27:59 +0500

    Fixed travis-build.sh.

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 cmake -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
-#cmake --build . && ctest
-
-#
-# Verbose output
-#
-cmake --build . -- -v && ctest
+cmake --build . && ctest
