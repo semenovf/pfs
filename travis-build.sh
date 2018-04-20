3ab5b0b4c31d4978d6469765b401e1cc0bb0e6f7
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-04-20 08:23:34 +0500

    Test regex on TravisCI (again).

--- travis-build.sh
+++ travis-build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cmake -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
 #cmake --build . && ctest
-cmake --build . && tests/tes-regex
+cmake --build . && tests/test-regex
