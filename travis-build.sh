bccf6807becd670d3af83d0348ac5c72c960745e
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-04-20 10:48:30 +0500

    Mdofied .travis.yml

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ulimit -c unlimited
-cmake -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Debug .
-#cmake --build . && ctest
-cmake --build . && tests/test-regex
-
+cmake -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
+cmake --build . && ctest
+#cmake --build . && tests/test-regex
 #gdb tests/test-regex core -ex "bt"
