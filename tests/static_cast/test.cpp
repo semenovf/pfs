6fbe683dc66ce4f5df2dec8834095889b842a4e1
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-04-22 16:04:27 +0500

    Test static_cast on TravisCI

--- tests/static_cast/test.cpp
+++ tests/static_cast/test.cpp
@@ -1,5 +1,6 @@
 #include <pfs/test.hpp>
 #include <pfs/types.hpp>
+#include <pfs/limits.hpp>
 
 ssize_t available ()
 {
@@ -17,8 +18,11 @@ int main ()
 
     ssize_t i1 = 2614;
     TEST_OK(static_cast<size_t>(i1) == 2614);
-
     read(available());
+
+    size_t navailable = pfs::integral_cast_check<size_t>(available());
+    std::cout << "navailable=" << navailable << std::endl;
+    TEST_OK(navailable == 2614);
 
     return END_TESTS;
 }
