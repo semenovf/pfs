aa51713397052e58806fd48d0f4a26883f7da8e4
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-04-22 15:38:08 +0500

    Test static_cast on TravisCI

--- travis-build.sh
+++ travis-build.sh
@@ -2,5 +2,7 @@
 ulimit -c unlimited
 cmake -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
 #cmake --build . && ctest
-cmake --build . && tests/test-io-file
+#cmake --build . && tests/test-io-file
+cmake --build . && tests/test-static_cast
+
 #gdb tests/test-regex core -ex "bt"
