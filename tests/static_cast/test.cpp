33c45b6be9682acabe07f28e404758a76614c188
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-04-22 15:53:18 +0500

    Test static_cast on TravisCI

--- tests/static_cast/test.cpp
+++ tests/static_cast/test.cpp
@@ -1,12 +1,24 @@
 #include <pfs/test.hpp>
 #include <pfs/types.hpp>
 
+ssize_t available ()
+{
+    return 2614;
+}
+
+void read (size_t n)
+{
+    TEST_OK(n == 2614);
+}
+
 int main ()
 {
-    BEGIN_TESTS(1);
+    BEGIN_TESTS(2);
 
-    ssize_t i1 = 1234;
-    TEST_OK(static_cast<size_t>(i1) == 1234);
+    ssize_t i1 = 2614;
+    TEST_OK(static_cast<size_t>(i1) == 2614);
+
+    read(available());
 
     return END_TESTS;
 }
