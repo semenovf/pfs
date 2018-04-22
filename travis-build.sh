d4de54bb97d78f72b8db1d7a64ee7f9e6620b17e
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-04-22 12:18:17 +0500

    Test io-file on TravisCI

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ulimit -c unlimited
 cmake -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
-cmake --build . && ctest
-#cmake --build . && tests/test-io-device_manager
+#cmake --build . && ctest
+cmake --build . && tests/test-io-file
 #gdb tests/test-regex core -ex "bt"
