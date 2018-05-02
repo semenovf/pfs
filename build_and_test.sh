144aa5943d51d4bcafd66fe54b89ee0fb9652b7a
Fedor Semenov <fedor.v.semenovf@gmail.com>
2018-05-02 18:24:11 +0500

    Fixed Travis CI config

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ulimit -c unlimited
+#ulimit -c unlimited
 cmake -DCMAKE_CXX_STANDARD=${CXX_STANDARD} -DCMAKE_BUILD_TYPE=Release .
 cmake --build . && ctest
 #cmake --build . && tests/test-io-file
