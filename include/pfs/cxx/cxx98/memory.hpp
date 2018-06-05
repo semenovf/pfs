e5909fd6d412b94a752abe15e00967f670d69506
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-06-05 11:28:27 +0500

    Minor changes.

--- include/pfs/cxx/cxx98/memory.hpp
+++ include/pfs/cxx/cxx98/memory.hpp
@@ -2,13 +2,13 @@
 #include <pfs/config.h>
 #include <pfs/cxxlang.hpp>
 
-#if defined(HAVE_BOOST_SMART_PTR)
+#if HAVE_BOOST_SMART_PTR
 #   include "shared_ptr_boost.hpp"
 #else
 #   include "shared_ptr_custom.hpp"
 #endif
 
-#if defined(HAVE_BOOST_UNIQUE_PTR)
+#if HAVE_BOOST_UNIQUE_PTR
 #   include "unique_ptr_boost.hpp"
 #else
 #   include "unique_ptr_custom.hpp"
