3291f58f68f075a25142384c10023f6708f00d00
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-12-10 14:14:59 +0500

    Started using external unit test framework (catch1(2)) instead of internal.

--- tests/lexical_cast/parse_integral_part.cpp
+++ tests/lexical_cast/parse_integral_part.cpp
@@ -1,63 +1,51 @@
-#include <iostream>
-#include <pfs/test.hpp>
 #include <pfs/string.hpp>
 #include <pfs/lexical_cast/parse_number.hpp>
+#include "../catch.hpp"
 
 template <typename IntT>
-static bool parse_integral_part_helper (char const * s
-    , int radix
-    , ptrdiff_t endpos_offset
-    , pfs::error_code expected_ec
-    , IntT expected_result)
+struct test_data {
+    char const *    s;
+    int             radix;
+    ptrdiff_t       endpos_offset;
+    pfs::error_code expected_ec;
+    IntT            expected_result;
+};
+
+template <typename IntT>
+void run_tests (test_data<IntT> * data, int count)
 {
     typedef pfs::string::const_iterator char_iterator_type;
 
-    pfs::string str(s);
-    pfs::error_code ec;
-    char_iterator_type endpos;
-    IntT result = pfs::parse_integral_part<IntT>(str.cbegin()
-            , str.cend()
-            , & endpos
-            , radix
-            , ec);
+    for (int i = 0; i < count; i++) {
+        pfs::string str(data[i].s);
+        pfs::error_code ec;
+        char_iterator_type endpos;
 
-    if (result != expected_result) {
-        std::cout << "***ERROR: result not match: "
-                // multiplcation to 1 need to output char type
-                // as number not as symbol
-                << result * 1 << " != " << expected_result * 1
-                << std::endl;
-        return false;
+        IntT result = pfs::parse_integral_part<IntT>(str.cbegin()
+                , str.cend()
+                , & endpos
+                , data[i].radix
+                , ec);
+
+        // multiplcation to 1 need to output char type
+        // as number not as symbol
+        CHECK(result * 1 == data[i].expected_result * 1);
+        CHECK(ec == data[i].expected_ec);
+        CHECK(pfs::distance(str.cbegin(), endpos) == data[i].endpos_offset);
     }
-
-    if (ec != expected_ec) {
-        std::cout << "***ERROR: error code not match: "
-                << pfs::to_string(ec) << " != " << pfs::to_string(expected_ec)
-                << std::endl;
-        return false;
-    }
-
-    ptrdiff_t pos_offset = pfs::distance(str.cbegin(), endpos);
-
-    if (pos_offset != endpos_offset) {
-        std::cout << "***ERROR: endpos offset not match: "
-                << pos_offset << " != " << endpos_offset << std::endl;
-        return false;
-    }
-
-    return true;
 }
 
-void test_parse_integral_part ()
-{
-    ADD_TESTS(6);
+TEST_CASE("Parse integral part") {
+    SECTION("signed char") {
+        test_data<signed char> data[] = {
+              { "0", 10, 1, pfs::error_code(), 0 }
+            , { "1", 10, 1, pfs::error_code(), 1 }
+            , { "127", 10, 3, pfs::error_code(), 127 }
+            , { "+127", 10, 4, pfs::error_code(), 127 }
+            , { "-127", 10, 4, pfs::error_code(), -127 }
+            , { "-128", 10, 4, pfs::error_code(), -128 }
+        };
 
-    TEST_OK(parse_integral_part_helper<signed char>("0", 10, 1, pfs::error_code(), 0));
-    TEST_OK(parse_integral_part_helper<signed char>("1", 10, 1, pfs::error_code(), 1));
-    TEST_OK(parse_integral_part_helper<signed char>("127", 10, 3, pfs::error_code(), 127));
-    TEST_OK(parse_integral_part_helper<signed char>("+127", 10, 4, pfs::error_code(), 127));
-    TEST_OK(parse_integral_part_helper<signed char>("-127", 10, 4, pfs::error_code(), -127));
-    TEST_OK(parse_integral_part_helper<signed char>("-128", 10, 4, pfs::error_code(), -128));
+        run_tests<signed char>(data, sizeof(data) / sizeof(data[0]));
+    }
 }
-
-
--- tests/lexical_cast/test.cpp
+++ tests/lexical_cast/test.cpp
@@ -1,32 +1,4 @@
-#include <pfs/test.hpp>
-#include <pfs/string.hpp>
-#include <pfs/lexical_cast.hpp>
-//#include <pfs/rational.hpp>
-// #include "test_string_to_uint.hpp"
-// #include "test_string_to_int.hpp"
-// #include "test_string_to_real.hpp"
-// #include "test_string_to_rational.hpp"
-// #include "test_lexical_cast_uint.hpp"
-// #include "test_lexical_cast_int.hpp"
-// #include "test_lexical_cast_real.hpp"
-
-extern void test_parse_digits ();
-extern void test_parse_integral_part ();
-
-int main ()
-{
-    BEGIN_TESTS(0);
-
-    test_parse_digits();
-    test_parse_integral_part();
-//     test_string_to_uint();
-//     test_string_to_int();
-//     test_string_to_real<float>();
-//     test_string_to_real<double>();
-//     test_string_to_rational();
-//     test_lexical_cast_uint();
-//     test_lexical_cast_int();
-//     test_lexical_cast_float();
-
-    return END_TESTS;
-}
+//
+// TODO OBSOLETE Source
+// Delete it after transition of all tests for use Catch1(2).
+//
