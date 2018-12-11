8c615fa10f0caad1c9c22d77cec5e9bd174a490c
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-12-11 16:12:00 +0500

    Implementing tests for integral.

--- tests/integral/parse_integral_part.cpp
+++ tests/integral/parse_integral_part.cpp
@@ -1,4 +1,4 @@
-#include <cstdlib> // for random
+#include <cstdlib> // for random, strtol
 #include <ctime>
 #include <pfs/string.hpp>
 #include <pfs/integral.hpp>
@@ -8,8 +8,14 @@
 
 typedef pfs::string::const_iterator char_iterator_type;
 
+//
+// Randomly generate test string in format:
+//
+// *3ws [ '+' / '-' ] ?nonzero_digit *32digit *3'-'
+//
 pfs::string generate_integral_part (int radix)
 {
+    char const * nonzerodigits = "123456789abcdefjhijklmnopqrstuvwxyz";
     char const * digits = "0123456789abcdefjhijklmnopqrstuvwxyz";
     pfs::string result;
 
@@ -25,6 +31,9 @@ pfs::string generate_integral_part (int radix)
 
     if (sign_index > 0)
         result.push_back(sign[sign_index]);
+
+//     int nonzerodigit_index = std::rand() % (radix - 1);
+//     result.push_back(digits[nonzerodigit_index]);
 
     int digit_count = std::rand() % 32;
 
@@ -42,6 +51,40 @@ pfs::string generate_integral_part (int radix)
 }
 
 template <typename IntT>
+IntT stdtoi (pfs::string const & str, pfs::string::const_iterator * endpos, int radix);
+
+template <>
+long int stdtoi<long int> (pfs::string const & str, pfs::string::const_iterator * endpos, int radix)
+{
+    char * endptr;
+    long int result = std::strtol(str.c_str(), & endptr, radix);
+    *endpos = pfs::string::const_iterator(endptr);
+    return result;
+}
+
+template <typename IntT>
+void compare (pfs::string const & str, int radix)
+{
+    pfs::string::const_iterator expected_endpos;
+    IntT expected_result = stdtoi<IntT>(str.c_str(), & expected_endpos, radix);
+
+    pfs::error_code ec;
+    pfs::string::const_iterator endpos;
+    IntT result = pfs::parse_integral_part<IntT>(str.cbegin()
+                , str.cend()
+                , ec
+                , & endpos
+                , radix);
+
+    CHECK(expected_result == result);
+
+    if (expected_result != result) {
+        std::cout << "***Error with string \"" << str
+                << "\" and radix = " << radix << std::endl;
+    }
+}
+
+template <typename IntT>
 struct test_data {
     char const *    s;
     int             radix;
@@ -51,7 +94,7 @@ struct test_data {
 };
 
 template <typename IntT>
-void run_syntatic_tests (test_data<IntT> * data, int count)
+void run_synthetic_tests (test_data<IntT> * data, int count)
 {
     for (int i = 0; i < count; i++) {
         pfs::string str(data[i].s);
@@ -77,22 +120,23 @@ void run_generated_tests (int count)
 {
     for (int radix = 2; radix <= 36; radix++) {
         for (int i = 0; i < count; i++) {
-            pfs::string s = generate_integral_part(radix);
-            pfs::error_code ec;
-            char_iterator_type endpos;
-
-            IntT result = pfs::parse_integral_part<IntT>(s.cbegin()
-                    , s.cend()
-                    , ec
-                    , & endpos
-                    , radix);
-
-            std::cout << "source=\"" << s << '"'
-                    << " result=[" << 1 * result << ']'
-                    << " radix=" << radix
-                    << " endpos=" << pfs::distance(s.cbegin(), endpos)
-                    << " error=" << '"' <<  pfs::to_string(ec) << '"'
-                    << std::endl;
+            pfs::string str = generate_integral_part(radix);
+            compare<IntT>(str, radix);
+//             pfs::error_code ec;
+//             char_iterator_type endpos;
+//
+//             IntT result = pfs::parse_integral_part<IntT>(s.cbegin()
+//                     , s.cend()
+//                     , ec
+//                     , & endpos
+//                     , radix);
+//
+//             std::cout << "source=\"" << s << '"'
+//                     << " result=[" << 1 * result << ']'
+//                     << " radix=" << radix
+//                     << " endpos=" << pfs::distance(s.cbegin(), endpos)
+//                     << " error=" << '"' <<  pfs::to_string(ec) << '"'
+//                     << std::endl;
         }
     }
 }
@@ -102,20 +146,40 @@ TEST_CASE("Tests on predefined sources") {
 
     SECTION("signed char") {
         test_data<signed char> data[] = {
-              { "0", 10, 1, pfs::error_code(), 0 }
-            , { "1", 10, 1, pfs::error_code(), 1 }
-            , { "127", 10, 3, pfs::error_code(), 127 }
-            , { "+127", 10, 4, pfs::error_code(), 127 }
+              {    "0", 10, 1, pfs::error_code(),    0 }
+            , {    "1", 10, 1, pfs::error_code(),    1 }
+            , {  "127", 10, 3, pfs::error_code(),  127 }
+            , { "+127", 10, 4, pfs::error_code(),  127 }
             , { "-127", 10, 4, pfs::error_code(), -127 }
             , { "-128", 10, 4, pfs::error_code(), -128 }
         };
 
-        run_syntatic_tests<signed char>(data, sizeof(data) / sizeof(data[0]));
+        run_synthetic_tests<signed char>(data, sizeof(data) / sizeof(data[0]));
+    }
+
+    SECTION("char") {
+        test_data<char> data[] = {
+              {    "0", 10, 1, pfs::error_code(),    0 }
+            , {    "1", 10, 1, pfs::error_code(),    1 }
+            , {  "127", 10, 3, pfs::error_code(),  127 }
+            , { "+127", 10, 4, pfs::error_code(),  127 }
+            , { "-127", 10, 4, pfs::error_code(), -127 }
+            , { "-128", 10, 4, pfs::error_code(), -128 }
+        };
+
+        run_synthetic_tests<char>(data, sizeof(data) / sizeof(data[0]));
     }
 }
 
 TEST_CASE("Tests on generated sources") {
-    run_generated_tests<signed char>(10);
-    run_generated_tests<char>(10);
-    run_generated_tests<long>(10);
+//     run_generated_tests<signed char>(10);
+//     run_generated_tests<char>(10);
+//     run_generated_tests<long>(10);
+
+
+    //run_generated_tests<long int>(10);
+
+    //compare<long int>(pfs::string("-1100001000111001--"), 2);
+    compare<long int>(pfs::string("-1100001000--"), 2);
+    //compare<long int>(pfs::string("1100001000--"), 2);
 }
