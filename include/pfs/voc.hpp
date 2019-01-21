bf34c7f946c82fd62739a08021c46f57c4c7c5d6
Fedor Semenov <fedor.v.semenov@gmail.com>
2019-01-21 10:18:57 +0500

    Added vocabular (voc) class and unit tests.

---
+++ include/pfs/voc.hpp
@@ -0,0 +1,103 @@
+#pragma once
+#include <pfs/string.hpp>
+#include <pfs/compare.hpp>
+
+namespace pfs {
+
+/**
+ */
+template <typename StructEnumT>
+class voc
+{
+public:
+    /**
+     * @fn voc (voc const & other);
+     * @brief Copy constructor.
+     */
+
+    /**
+     * @fn voc & operator = (voc const & other);
+     * @brief Copy assignable operator.
+     */
+
+    voc (typename StructEnumT::value_enum value = StructEnumT::__BAD_VALUE__)
+        : _value(value)
+    {}
+
+    voc & operator = (typename StructEnumT::value_enum value)
+    {
+        _value = value;
+        return *this;
+    }
+
+    typename StructEnumT::value_enum value () const
+    {
+        return _value;
+    }
+
+    bool valid () const
+    {
+        return valid(_value);
+    }
+
+    static bool valid (int value);
+
+private:
+    typename StructEnumT::value_enum _value;
+};
+
+template <typename StructEnumT>
+inline bool operator == (voc<StructEnumT> const & lhs, voc<StructEnumT> const & rhs)
+{
+    return lhs.value() == rhs.value();
+}
+
+template <typename StructEnumT>
+inline bool operator == (voc<StructEnumT> const & lhs, typename StructEnumT::value_enum rhs)
+{
+    return lhs.value() == rhs;
+}
+
+template <typename StructEnumT>
+inline bool operator == (typename StructEnumT::value_enum lhs, voc<StructEnumT> const & rhs)
+{
+    return lhs == rhs.value();
+}
+
+template <typename StructEnumT>
+inline bool operator < (voc<StructEnumT> const & lhs, voc<StructEnumT> const & rhs)
+{
+    return lhs.value() < rhs.value();
+}
+
+template <typename StructEnumT>
+inline bool operator < (voc<StructEnumT> const & lhs, typename StructEnumT::value_enum rhs)
+{
+    return lhs.value() < rhs;
+}
+
+template <typename StructEnumT>
+inline bool operator < (typename StructEnumT::value_enum lhs, voc<StructEnumT> const & rhs)
+{
+    return lhs < rhs.value();
+}
+
+template <typename StructEnumT>
+string to_string (voc<StructEnumT> const & voc);
+
+template <typename StructEnumT>
+string to_acronym (voc<StructEnumT> const & voc);
+
+template <typename StructEnumT>
+inline voc<StructEnumT> make_voc (int value)
+{
+    return voc<StructEnumT>::valid(value)
+            ? voc<StructEnumT>(static_cast<typename StructEnumT::value_enum>(value))
+            : voc<StructEnumT>();
+}
+
+template <typename StructEnumT>
+voc<StructEnumT> make_voc (string const & s);
+
+} // namespace pfs
+
---
+++ tests/voc/test.cpp
@@ -0,0 +1,231 @@
+#include <pfs/string.hpp>
+#include <pfs/voc.hpp>
+#include "../catch.hpp"
+
+// Enum struct declaration
+struct counter_enum
+{
+    enum value_enum {
+          ZERO
+        , ONE
+        , TWO
+        , THREE
+        , __BAD_VALUE__
+    };
+};
+
+static pfs::string ACRONYMS[] = {
+      pfs::string("0")
+    , pfs::string("1")
+    , pfs::string("2")
+    , pfs::string("3")
+    , pfs::string("?")
+};
+
+static pfs::string STRINGS[] = {
+      pfs::string("Zero")
+    , pfs::string("One")
+    , pfs::string("Two")
+    , pfs::string("Three")
+    , pfs::string("?")
+};
+
+//
+// Specializations
+//
+namespace pfs {
+
+// // voc::name() methos specialization
+// template <>
+// pfs::string const & voc<counter_enum>::name ()
+// {
+//     static pfs::string __name("Counter");
+//     return __name;
+// }
+//
+// to_acronym() function specialization
+template <>
+pfs::string to_acronym<counter_enum> (voc<counter_enum> const & voc)
+{
+    return ACRONYMS[voc.value()];
+}
+
+// to_string() function specialization
+template <>
+pfs::string to_string<counter_enum> (voc<counter_enum> const & voc)
+{
+    return STRINGS[voc.value()];
+}
+
+
+// voc::valid() function specialization
+template <>
+bool voc<counter_enum>::valid (int value)
+{
+    return value >= counter_enum::ZERO && value < counter_enum::__BAD_VALUE__;
+}
+
+template <>
+voc<counter_enum> make_voc (pfs::string const & s)
+{
+    if (!s.empty()) {
+        for (int i = counter_enum::ZERO; i < counter_enum::__BAD_VALUE__; i++) {
+            if (ACRONYMS[i] == s || STRINGS[i] == s)
+                return make_voc<counter_enum>(i);
+        }
+    }
+
+    return voc<counter_enum>();
+}
+
+} // namespace pfs
+
+typedef pfs::voc<counter_enum> voc_t;
+
+inline std::ostream & operator << (std::ostream & os, voc_t const & value)
+{
+    os << value.value();
+    return os;
+}
+
+
+TEST_CASE("Default constructor") {
+    voc_t voc;
+    CHECK_FALSE(voc.valid());
+}
+
+TEST_CASE("Constructors and assign operators") {
+    voc_t zero(counter_enum::ZERO);
+    voc_t zero_clone(zero);
+    CHECK(zero.valid());
+    CHECK(zero_clone.valid());
+
+    voc_t one(counter_enum::ONE);
+    voc_t one_clone(one);
+    CHECK(one.valid());
+    CHECK(one_clone.valid());
+
+    voc_t two(counter_enum::TWO);
+    voc_t two_clone(two);
+    CHECK(two.valid());
+    CHECK(two_clone.valid());
+
+    voc_t three(counter_enum::THREE);
+    voc_t three_clone(three);
+    CHECK(three.valid());
+    CHECK(three_clone.valid());
+
+    voc_t bad(counter_enum::__BAD_VALUE__);
+    voc_t bad_clone(bad);
+    CHECK_FALSE(bad.valid());
+    CHECK_FALSE(bad_clone.valid());
+
+    voc_t voc;
+    CHECK_FALSE(voc.valid());
+
+    voc = zero;
+    CHECK(voc.value() == counter_enum::ZERO);
+
+    voc = one;
+    CHECK(voc.value() == counter_enum::ONE);
+
+    voc = two;
+    CHECK(voc.value() == counter_enum::TWO);
+
+    voc = three;
+    CHECK(voc.value() == counter_enum::THREE);
+
+    voc = bad;
+    CHECK(voc.value() == counter_enum::__BAD_VALUE__);
+}
+
+TEST_CASE("Makers") {
+    voc_t bad_voc = pfs::make_voc<counter_enum>(-1);
+    CHECK(bad_voc.value() == counter_enum::__BAD_VALUE__);
+
+    bad_voc = pfs::make_voc<counter_enum>(100);
+    CHECK(bad_voc.value() == counter_enum::__BAD_VALUE__);
+
+    voc_t zero = pfs::make_voc<counter_enum>(0);
+    CHECK(zero.value() == counter_enum::ZERO);
+
+    voc_t one = pfs::make_voc<counter_enum>(1);
+    CHECK(one.value() == counter_enum::ONE);
+
+    voc_t two = pfs::make_voc<counter_enum>(2);
+    CHECK(two.value() == counter_enum::TWO);
+
+    voc_t three = pfs::make_voc<counter_enum>(3);
+    CHECK(three.value() == counter_enum::THREE);
+
+
+    CHECK(pfs::make_voc<counter_enum>("-1").value() == counter_enum::__BAD_VALUE__);
+    CHECK(pfs::make_voc<counter_enum>("Bad Value").value() == counter_enum::__BAD_VALUE__);
+
+    CHECK(pfs::make_voc<counter_enum>("0").value() == 0);
+    CHECK(pfs::make_voc<counter_enum>("Zero").value() == 0);
+
+    CHECK(pfs::make_voc<counter_enum>("1").value() == 1);
+    CHECK(pfs::make_voc<counter_enum>("One").value() == 1);
+
+    CHECK(pfs::make_voc<counter_enum>("2").value() == 2);
+    CHECK(pfs::make_voc<counter_enum>("Two").value() == 2);
+
+    CHECK(pfs::make_voc<counter_enum>("3").value() == 3);
+    CHECK(pfs::make_voc<counter_enum>("Three").value() == 3);
+}
+
+TEST_CASE("To string conversions") {
+    CHECK(pfs::to_acronym(voc_t()) == "?");
+    CHECK(pfs::to_string(voc_t()) == "?");
+
+    CHECK(pfs::to_acronym(voc_t(counter_enum::ZERO)) == "0");
+    CHECK(pfs::to_string(voc_t(counter_enum::ZERO)) == "Zero");
+
+    CHECK(pfs::to_acronym(voc_t(counter_enum::ONE)) == "1");
+    CHECK(pfs::to_string(voc_t(counter_enum::ONE)) == "One");
+
+    CHECK(pfs::to_acronym(voc_t(counter_enum::TWO)) == "2");
+    CHECK(pfs::to_string(voc_t(counter_enum::TWO)) == "Two");
+
+    CHECK(pfs::to_acronym(voc_t(counter_enum::THREE)) == "3");
+    CHECK(pfs::to_string(voc_t(counter_enum::THREE)) == "Three");
+}
+
+TEST_CASE("Comparisons") {
+    voc_t zero(counter_enum::ZERO);
+    voc_t one(counter_enum::ONE);
+    voc_t two(counter_enum::TWO);
+    voc_t three(counter_enum::THREE);
+    voc_t bad(counter_enum::__BAD_VALUE__);
+
+    CHECK(zero == zero);
+    CHECK(one == one);
+    CHECK(two == two);
+    CHECK(three == three);
+    CHECK(bad == bad);
+
+    CHECK(zero != one);
+    CHECK(zero < one);
+    CHECK(zero <= one);
+    CHECK(zero <= zero);
+    CHECK(one > zero);
+    CHECK(one >= zero);
+    CHECK(one >= one);
+
+    CHECK(zero != counter_enum::ONE);
+    CHECK(zero < counter_enum::ONE);
+    CHECK(zero <= counter_enum::ONE);
+    CHECK(zero <= counter_enum::ZERO);
+    CHECK(one > counter_enum::ZERO);
+    CHECK(one >= counter_enum::ZERO);
+    CHECK(one >= counter_enum::ZERO);
+
+    CHECK(counter_enum::ZERO != one);
+    CHECK(counter_enum::ZERO < one);
+    CHECK(counter_enum::ZERO <= one);
+    CHECK(counter_enum::ZERO <= zero);
+    CHECK(counter_enum::ONE > zero);
+    CHECK(counter_enum::ONE >= zero);
+    CHECK(counter_enum::ONE >= one);
+}
