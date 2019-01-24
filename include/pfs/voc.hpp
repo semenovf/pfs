071114893d67f54d7b5914ce78de9b5a5393b452
Fedor Semenov <fedor.v.semenov@gmail.com>
2019-01-24 22:25:58 +0500

    Fixed voc for C++98 and C++11.

--- include/pfs/voc.hpp
+++ include/pfs/voc.hpp
@@ -7,20 +7,42 @@
 
 namespace pfs {
 
-/**
- */
+#if __cplusplus >= 201103L
+template <typename StructEnumT, bool = std::is_enum<StructEnumT>::value>
+struct voc_helper
+{
+    using underlying_type = typename std::underlying_type<StructEnumT>::type;
+    using type = StructEnumT;
+};
+
+template <typename StructEnumT>
+struct voc_helper<StructEnumT, false>
+{
+    using underlying_type = typename std::underlying_type<typename StructEnumT::value_enum>::type;
+    using type = typename StructEnumT::value_enum;
+};
+#else
+template <typename StructEnumT>
+struct voc_helper
+{
+    typedef typename size_type_n<sizeof(typename StructEnumT::value_enum)>::signed_type underlying_type;
+    typedef typename StructEnumT::value_enum type;
+};
+#endif
+
 template <typename StructEnumT>
 class voc
 {
 public:
-    typedef typename size_type_n<sizeof(typename StructEnumT::value_enum)>::signed_type native_type;
+    typedef typename voc_helper<StructEnumT>::underlying_type underlying_type;
+    typedef typename voc_helper<StructEnumT>::type type;
 
 private:
-    static const native_type bad_value ()
+    static underlying_type const bad_value ()
     {
-        return numeric_limits<native_type>::max();
+        return pfs::numeric_limits<underlying_type>::max();
     }
-
+    
 public:
     /**
      * @fn voc (voc const & other);
@@ -35,23 +57,23 @@ public:
     /**
      */
     voc () : _value(bad_value()) {}
-
-    explicit voc (typename StructEnumT::value_enum value)
-        : _value(static_cast<native_type>(value))
+    
+    explicit voc (type value)
+        : _value(static_cast<underlying_type>(value))
     {}
 
-    voc & operator = (typename StructEnumT::value_enum value)
+    voc & operator = (type value)
     {
-        _value = static_cast<native_type>(value);
+        _value = static_cast<underlying_type>(value);
         return *this;
     }
-
-    typename StructEnumT::value_enum value () const
+    
+    type value () const
     {
         PFS_THROW_EXPR(valid());
-        return static_cast<typename StructEnumT::value_enum>(_value);
+        return static_cast<type>(_value);
     }
-
+    
     bool valid () const
     {
         return _value != bad_value();
@@ -64,10 +86,10 @@ public:
     string to_acronym () const;
 
     static string name ();
-    static bool valid (native_type value);
-
+    static bool valid (underlying_type value);
+    
 private:
-    native_type _value;
+    underlying_type _value;
 };
 
 template <typename StructEnumT>
@@ -77,13 +99,13 @@ inline bool operator == (voc<StructEnumT> const & lhs, voc<StructEnumT> const & 
 }
 
 template <typename StructEnumT>
-inline bool operator == (voc<StructEnumT> const & lhs, typename StructEnumT::value_enum rhs)
+inline bool operator == (voc<StructEnumT> const & lhs, typename voc<StructEnumT>::type rhs)
 {
     return lhs.value() == rhs;
 }
 
 template <typename StructEnumT>
-inline bool operator == (typename StructEnumT::value_enum lhs, voc<StructEnumT> const & rhs)
+inline bool operator == (typename voc<StructEnumT>::type lhs, voc<StructEnumT> const & rhs)
 {
     return lhs == rhs.value();
 }
@@ -95,22 +117,22 @@ inline bool operator < (voc<StructEnumT> const & lhs, voc<StructEnumT> const & r
 }
 
 template <typename StructEnumT>
-inline bool operator < (voc<StructEnumT> const & lhs, typename StructEnumT::value_enum rhs)
+inline bool operator < (voc<StructEnumT> const & lhs, typename voc<StructEnumT>::type rhs)
 {
     return lhs.value() < rhs;
 }
 
 template <typename StructEnumT>
-inline bool operator < (typename StructEnumT::value_enum lhs, voc<StructEnumT> const & rhs)
+inline bool operator < (typename voc<StructEnumT>::type lhs, voc<StructEnumT> const & rhs)
 {
     return lhs < rhs.value();
 }
 
 template <typename StructEnumT>
-inline voc<StructEnumT> make_voc (typename voc<StructEnumT>::native_type value)
+inline voc<StructEnumT> make_voc (typename voc<StructEnumT>::underlying_type value)
 {
     return voc<StructEnumT>::valid(value)
-            ? voc<StructEnumT>(static_cast<typename StructEnumT::value_enum>(value))
+            ? voc<StructEnumT>(static_cast<typename voc<StructEnumT>::type>(value))
             : voc<StructEnumT>();
 }
 
@@ -124,4 +146,3 @@ inline string to_string (voc<StructEnumT> const & voc)
 }
 
 } // namespace pfs
-
--- tests/voc/test.cpp
+++ tests/voc/test.cpp
@@ -70,7 +70,7 @@ string voc<counter_enum>::to_string () const
 }
 
 template <>
-bool voc<counter_enum>::valid (native_type value)
+bool voc<counter_enum>::valid (underlying_type value)
 {
     return value >= counter_enum::ZERO && value <= counter_enum::THREE;
 }
@@ -90,44 +90,42 @@ voc<counter_enum> make_voc (string const & s)
 
 #if __cplusplus >= 201103L
 
-// voc_name() function specialization
 template <>
-string voc_name<counter_enum_class> ()
+string voc<counter_enum_class>::name ()
 {
     return NAME2;
 }
 
-// to_acronym() function specialization
+// to_acronym() method specialization
 template <>
-pfs::string to_acronym<counter_enum_class> (voc<counter_enum_class> const & voc)
+string voc<counter_enum_class>::to_acronym () const
 {
-    return ACRONYMS[static_cast<int>(voc.value())];
+    return valid() ? ACRONYMS[_value] : string();
 }
 
-// to_string() function specialization
+// to_string() method specialization
 template <>
-pfs::string to_string<counter_enum_class> (voc<counter_enum_class> const & voc)
+string voc<counter_enum_class>::to_string () const
 {
-    return STRINGS[static_cast<int>(voc.value())];
-}
-
-// voc::valid() function specialization
-template <>
-bool voc<counter_enum_clas>::valid (int value)
-{
-    return value >= static_cast<int>(counter_enum_clas::ZERO)
-            && value < static_cast<int>(counter_enum_clas::__BAD_VALUE__);
+    return valid() ? STRINGS[_value] : string();
 }
 
 template <>
-voc<counter_enum_class> make_voc (pfs::string const & s)
+bool voc<counter_enum_class>::valid (underlying_type value)
+{
+    return value >= static_cast<underlying_type>(counter_enum::ZERO)
+            && value <= static_cast<underlying_type>(counter_enum::THREE);
+}
+
+template <>
+voc<counter_enum_class> make_voc (string const & s)
 {
     if (!s.empty()) {
-        for (int i = static_cast<int>(counter_enum_class::ZERO)
-                    ; i < static_cast<int>(counter_enum_class)::__BAD_VALUE__
-                    ; i++) {
+        int first = static_cast<int>(counter_enum_class::ZERO);
+        int last = static_cast<int>(counter_enum_class::THREE);
+        for (int i = first; i <= last; i++) {
             if (ACRONYMS[i] == s || STRINGS[i] == s)
-                return make_voc<counter_enum_clas>(i);
+                return make_voc<counter_enum_class>(i);
         }
     }
 
@@ -160,6 +158,13 @@ TEST_CASE("Default constructor") {
     voc_t voc;
     CHECK_FALSE(voc.valid());
 }
+
+#if __cplusplus >= 201103L
+TEST_CASE("Default constructor (C++11)") {
+    voc_class_t voc;
+    CHECK_FALSE(voc.valid());
+}
+#endif
 
 TEST_CASE("Constructors and assign operators") {
     voc_t zero(counter_enum::ZERO);
@@ -206,6 +211,53 @@ TEST_CASE("Constructors and assign operators") {
     CHECK_THROWS(voc.value());
 }
 
+#if __cplusplus >= 201103L
+TEST_CASE("Constructors and assign operators (C++11)") {
+    voc_class_t zero(counter_enum_class::ZERO);
+    voc_class_t zero_clone(zero);
+    CHECK(zero.valid());
+    CHECK(zero_clone.valid());
+
+    voc_class_t one(counter_enum_class::ONE);
+    voc_class_t one_clone(one);
+    CHECK(one.valid());
+    CHECK(one_clone.valid());
+
+    voc_class_t two(counter_enum_class::TWO);
+    voc_class_t two_clone(two);
+    CHECK(two.valid());
+    CHECK(two_clone.valid());
+
+    voc_class_t three(counter_enum_class::THREE);
+    voc_class_t three_clone(three);
+    CHECK(three.valid());
+    CHECK(three_clone.valid());
+
+    voc_class_t bad;
+    voc_class_t bad_clone(bad);
+    CHECK_FALSE(bad.valid());
+    CHECK_FALSE(bad_clone.valid());
+
+    voc_class_t voc;
+    CHECK_FALSE(voc.valid());
+
+    voc = zero;
+    CHECK(voc.value() == counter_enum_class::ZERO);
+
+    voc = one;
+    CHECK(voc.value() == counter_enum_class::ONE);
+
+    voc = two;
+    CHECK(voc.value() == counter_enum_class::TWO);
+
+    voc = three;
+    CHECK(voc.value() == counter_enum_class::THREE);
+
+    voc = bad;
+    CHECK_THROWS(voc.value());
+}
+#endif    
+
 TEST_CASE("Makers") {
     voc_t bad_voc = pfs::make_voc<counter_enum>(-1);
     CHECK_THROWS(bad_voc.value());
@@ -242,6 +294,43 @@ TEST_CASE("Makers") {
     CHECK(pfs::make_voc<counter_enum>("Three").value() == 3);
 }
 
+#if __cplusplus >= 201103L
+TEST_CASE("Makers (C++11)") {
+    voc_class_t bad_voc = pfs::make_voc<counter_enum_class>(-1);
+    CHECK_THROWS(bad_voc.value());
+
+    bad_voc = pfs::make_voc<counter_enum_class>(100);
+    CHECK_THROWS(bad_voc.value());
+
+    voc_class_t zero = pfs::make_voc<counter_enum_class>(0);
+    CHECK(zero.value() == counter_enum_class::ZERO);
+
+    voc_class_t one = pfs::make_voc<counter_enum_class>(1);
+    CHECK(one.value() == counter_enum_class::ONE);
+
+    voc_class_t two = pfs::make_voc<counter_enum_class>(2);
+    CHECK(two.value() == counter_enum_class::TWO);
+
+    voc_class_t three = pfs::make_voc<counter_enum_class>(3);
+    CHECK(three.value() == counter_enum_class::THREE);
+
+    CHECK_THROWS(pfs::make_voc<counter_enum_class>("-1").value());
+    CHECK_THROWS(pfs::make_voc<counter_enum_class>("Bad Value").value());
+
+    CHECK(pfs::make_voc<counter_enum_class>("0").value() == counter_enum_class::ZERO);
+    CHECK(pfs::make_voc<counter_enum_class>("Zero").value() == counter_enum_class::ZERO);
+
+    CHECK(pfs::make_voc<counter_enum_class>("1").value() == counter_enum_class::ONE);
+    CHECK(pfs::make_voc<counter_enum_class>("One").value() == counter_enum_class::ONE);
+
+    CHECK(pfs::make_voc<counter_enum_class>("2").value() == counter_enum_class::TWO);
+    CHECK(pfs::make_voc<counter_enum_class>("Two").value() == counter_enum_class::TWO);
+
+    CHECK(pfs::make_voc<counter_enum_class>("3").value() == counter_enum_class::THREE);
+    CHECK(pfs::make_voc<counter_enum_class>("Three").value() == counter_enum_class::THREE);
+}
+#endif    
+
 TEST_CASE("To string conversions") {
     CHECK(voc_t().to_acronym().empty());
     CHECK(voc_t().to_string().empty());
@@ -258,6 +347,25 @@ TEST_CASE("To string conversions") {
     CHECK(voc_t(counter_enum::THREE).to_acronym() == "3");
     CHECK(voc_t(counter_enum::THREE).to_string() == "Three");
 }
+
+#if __cplusplus >= 201103L
+TEST_CASE("To string conversions (C++11)") {
+    CHECK(voc_class_t().to_acronym().empty());
+    CHECK(voc_class_t().to_string().empty());
+
+    CHECK(voc_class_t(counter_enum_class::ZERO).to_acronym() == "0");
+    CHECK(voc_class_t(counter_enum_class::ZERO).to_string() == "Zero");
+
+    CHECK(voc_class_t(counter_enum_class::ONE).to_acronym() == "1");
+    CHECK(voc_class_t(counter_enum_class::ONE).to_string() == "One");
+
+    CHECK(voc_class_t(counter_enum_class::TWO).to_acronym() == "2");
+    CHECK(voc_class_t(counter_enum_class::TWO).to_string() == "Two");
+
+    CHECK(voc_class_t(counter_enum_class::THREE).to_acronym() == "3");
+    CHECK(voc_class_t(counter_enum_class::THREE).to_string() == "Three");
+}
+#endif    
 
 TEST_CASE("Comparisons") {
     voc_t zero(counter_enum::ZERO);
@@ -297,6 +405,53 @@ TEST_CASE("Comparisons") {
     CHECK(counter_enum::ONE >= one);
 }
 
+#if __cplusplus >= 201103L
+TEST_CASE("Comparisons (C++11)") {
+    voc_class_t zero(counter_enum_class::ZERO);
+    voc_class_t one(counter_enum_class::ONE);
+    voc_class_t two(counter_enum_class::TWO);
+    voc_class_t three(counter_enum_class::THREE);
+    voc_class_t bad;
+
+    CHECK(zero == zero);
+    CHECK(one == one);
+    CHECK(two == two);
+    CHECK(three == three);
+    CHECK_THROWS(bad == bad);
+
+    CHECK(zero != one);
+    CHECK(zero < one);
+    CHECK(zero <= one);
+    CHECK(zero <= zero);
+    CHECK(one > zero);
+    CHECK(one >= zero);
+    CHECK(one >= one);
+
+    CHECK(zero != counter_enum_class::ONE);
+    CHECK(zero < counter_enum_class::ONE);
+    CHECK(zero <= counter_enum_class::ONE);
+    CHECK(zero <= counter_enum_class::ZERO);
+    CHECK(one > counter_enum_class::ZERO);
+    CHECK(one >= counter_enum_class::ZERO);
+    CHECK(one >= counter_enum_class::ZERO);
+
+    CHECK(counter_enum_class::ZERO != one);
+    CHECK(counter_enum_class::ZERO < one);
+    CHECK(counter_enum_class::ZERO <= one);
+    CHECK(counter_enum_class::ZERO <= zero);
+    CHECK(counter_enum_class::ONE > zero);
+    CHECK(counter_enum_class::ONE >= zero);
+    CHECK(counter_enum_class::ONE >= one);
+}
+#endif    
+
+
 TEST_CASE("Test vocabulary name") {
     CHECK(voc_t::name() == NAME1);
 }
+
+#if __cplusplus >= 201103L
+TEST_CASE("Test vocabulary name (C++11)") {
+    CHECK(voc_class_t::name() == NAME2);
+}
+#endif    
